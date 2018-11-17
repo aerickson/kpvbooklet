b7de17d45ff19d74a4e98133869624f162e59f34
Andrew Erickson <aerickson@gmail.com>
2018-11-16 23:53:37 -0800

    make the build script fail on error

--- build-updates.sh
+++ build-updates.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+set -e
+
 HACKNAME="kpvbooklet"
 PKGNAME="${HACKNAME}"
-PKGVER="0.6.6"
+PKGVER="0.6.7.pre"
 
 # We need kindletool (https://github.com/NiLuJe/KindleTool) in $PATH
 if (( $(kindletool version | wc -l) == 1 )) ; then
